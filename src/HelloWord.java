c712a079984b4126d6b1fc5f97bfe3fe80286957
jerryshann <291936135@qq.com>
2016-02-28 11:57:12 +0800

    First commit from batch

---
+++ src/HelloWord.java
@@ -0,0 +1,11 @@
+import android.app.Activity;
+
+public class HelloWorld extends Activity{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
